589387fd826421520cb5c02df239e6cc8026a73c
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2021-05-11 23:17:48 +0500

    Scroll to the top

--- js/index.js
+++ js/index.js
@@ -9,4 +9,10 @@
       prevEl: '.slider__button--prev',
     }
   })
+
+  var buttonUp = document.querySelector('.footer__button-up')
+  buttonUp.addEventListener('click', function (event) {
+    event.preventDefault()
+    window.scrollTo(0, 0)
+  })
 })()
