19705a76f52b041739a75b1044886d8af56bd7cc
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2021-05-11 17:30:44 +0500

    Add swiper slider

---
+++ js/index.js
@@ -0,0 +1,12 @@
+(function() {
+  new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__button--next',
+      prevEl: '.slider__button--prev',
+    }
+  })
+})()
